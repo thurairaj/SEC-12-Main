027ad1e791ea338c2fb0e8e97e22b5b78dcd9aef
Thurairaj L <thurairaj.letchumanan@gmail.com>
2026-01-22 21:47:10 +0800

    Week Three

--- express-server/index.js
+++ express-server/index.js
@@ -1,35 +1,72 @@
-const express = require("express");
-
-function logger(req, res, next) {
-	console.log(req.method, req.url, req.body);
-	next();
-}
+const express = require('express');
 
 const app = express();
+const users = []; // substituted by db
+
+// TODO
 app.use(express.json());
-app.use(logger)
 
+/*
+	CRUD operation
+ */
 
-
-app.post("/change", (req, res) => {
-	res.send("Hello World!");
-	res.end();
+// CREATE
+app.post('/user', (req, res) => {
+	const userCreationRequest = req.body;
+	const user = {};
+	user.name = userCreationRequest.name;
+	user.username = userCreationRequest.username;
+	user.age = userCreationRequest.age;
+	user.phoneNumber = userCreationRequest.phoneNumber;
+	user.email = userCreationRequest.email;
+	user.id = Date.now().toString();
+	users.push(user);
+	res.status(200).json(user)
 })
 
-app.post("/loading", (req, res) => {
-	const payload = req.body;
-	const result = {
-		cookedRice: payload.rice + 1,
+// READ ALL
+app.get('/user',  (req, res) => {
+	res.status(200).json(users)
+})
+
+// READ ONE USER?
+app.get('/user/:id', (req, res) => {
+	const userId = req.params.id;
+	const user = users.find(user => user.id === userId)
+	if (user) {
+		res.status(200).json(user)
+	} else {
+		res.status(404).end()
 	}
-	res.send(result);
 })
 
-app.get("/something", (req, res) => {
-	res.send("Welcome to Door Something!");
+app.put('/user/:id', (req, res) => {
+	const userId = req.params.id;
+	const payload = req.body;
+	const user = users.find(user => user.id === userId)
+	if (user) {
+		user.name = payload.name;
+		res.status(200).json(user)
+	} else {
+		res.status(404).end()
+	}
 })
 
+app.delete('/user/:id', (req, res) => {
+	const userId = req.params.id;
+	const user = users.find(user => user.id === userId);
+	if (user) {
+		const indexOfUser = users.findIndex(user => user.id === userId)
+		users.splice(indexOfUser, 1);
+	}
+	res.status(200).json(user)
+})
 
 
 app.listen(3000, () => {
-	console.log("Server running on port 3000");
-});
+	console.log('Express server started');
+})
+/*
+	[browser] --- magic ---> [server = machine] ---> PORT
+ */
+
