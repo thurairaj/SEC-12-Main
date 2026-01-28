2d55ddf0852c76842ef377cebc460f892ad187c7
Thurairaj L <thurairaj.letchumanan@gmail.com>
2026-01-28 22:12:03 +0800

    booking system routes

--- booking-system/src/routes/index.js
+++ booking-system/src/routes/index.js
@@ -3,6 +3,6 @@ const userRoutes = require('./user.route');
 
 const router = express.Router();
 router.use('/users', userRoutes);
-
+//   /api/users
 
 module.exports = router;
--- booking-system/src/routes/user.route.js
+++ booking-system/src/routes/user.route.js
@@ -2,6 +2,10 @@ const express = require('express');
 const userController = require('../controller/user.controller');
 const router = express.Router();
 
+
+// POST [/api/users]/
+//          \
+//         routes/index.js
 router.post('/', userController.createUser)
 
 module.exports = router;
