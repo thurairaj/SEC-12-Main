00dcc5613e5a829c1b947e01b1f550980ca8d399
Thurairaj L <thurairaj.letchumanan@gmail.com>
2026-03-09 21:25:20 +0800

    Bug fix

--- auth-expressjs/src/middleware/requireAuth.js
+++ auth-expressjs/src/middleware/requireAuth.js
@@ -3,7 +3,7 @@ const { config } = require('../config/env');
 
 function requireAuth(req, res, next) {
 	if (config.authStrategy === 'session') {
-		if(req.session?.userId) {
+		if(!req.session?.userId) {
 			return res.status(401).send({message: "Mot authenticated"})
 		}
 
