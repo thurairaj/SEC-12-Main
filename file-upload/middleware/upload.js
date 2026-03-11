b6862cc2c8c0c095e6e56c7133c0a2f089481c90
Thurairaj L <thurairaj.letchumanan@gmail.com>
2026-03-11 21:38:20 +0800

    file-upload

---
+++ file-upload/middleware/upload.js
@@ -0,0 +1,40 @@
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const uploadDirectory = path.join(__dirname, 'uploads');
+
+if (!fs.existsSync(uploadDirectory)) {
+	fs.mkdirSync(uploadDirectory, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, uploadDirectory);
+	},
+
+	filename: function (req, file, cb) {
+		const uniqueName = `${Date.now()}-${file.originalname}`
+		cb(null, uniqueName);
+	}
+})
+
+const fileFilter = (req, file, cb) => {
+	const allowedMimeTypes = ['image/png', 'image/jpeg'];
+
+	if (!allowedMimeTypes.includes(file.mimeType)) {
+		cb(new Error('Invalid file type'));
+	}
+
+	cb(null, true);
+}
+
+const upload = multer({
+	storage: storage,
+	fileFilter: fileFilter,
+	limit: {
+		fileSize: 1024 * 1024 * 1024
+	}
+})
+
+module.exports = upload;
