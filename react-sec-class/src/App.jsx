d8223302cd872f229a0e9362804a54606e318d52
Thurairaj L <thurairaj.letchumanan@gmail.com>
2026-03-30 20:21:01 +0800

    Add header

---
+++ react-sec-class/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from "./component/Header.jsx";
+
+function App() {
+
+  return (
+    <div>
+        <Header/>
+        <p>Hello World</p>
+    </div>
+  )
+}
+
+export default App
---
+++ react-sec-class/src/component/Header.jsx
@@ -0,0 +1,7 @@
+function Header() {
+	return <header>
+		<h1>Task Board</h1>
+	</header>
+}
+
+export default Header;
